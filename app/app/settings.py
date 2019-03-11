d635f4628586bfb3615d2a55f1f9d4bfe1b3147c
nobel6018 <nobel6018@gmail.com>
2019-03-11 22:38:47 +0900

    Feat: tag list end point

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,8 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    'rest_framework',
+    'rest_framework.authtoken',
+
     # local
     'core',
+    'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
@@ -78,6 +84,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'HOST': os.environ.get('DB_HOST'),
+    #     'NAME': os.environ.get('DB_NAME'),
+    #     'USER': os.environ.get('DB_USER'),
+    #     'PASSWORD': os.environ.get('DB_PASS'),
+    #     'PORT': '5432',
+    # }
 }
 
 # Password validation
